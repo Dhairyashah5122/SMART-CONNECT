ba37db42814a2dd5690327e63bc0d1cf428aecb9
Sanaz Tehrani <s.tehrani@westcliff.edu>
2025-08-14 09:20:02 +0000

    Survey Management add survety html from or here

--- src/app/surveys/page.tsx
+++ src/app/surveys/page.tsx
@@ -1,6 +1,7 @@
 import { SurveyList } from "@/components/surveys/survey-list";
 import { Button } from "@/components/ui/button";
 import { FilePlus2 } from "lucide-react";
+import Link from "next/link";
 
 export default function SurveysPage() {
   return (
@@ -9,9 +10,11 @@ export default function SurveysPage() {
         <h2 className="text-2xl font-bold tracking-tight">
           Survey Management
         </h2>
-        <Button variant="outline">
-          <FilePlus2 className="mr-2 h-4 w-4" />
-          Create Survey
+        <Button asChild variant="outline">
+          <Link href="/surveys/create">
+            <FilePlus2 className="mr-2 h-4 w-4" />
+            Create Survey
+          </Link>
         </Button>
       </div>
       <p className="text-muted-foreground max-w-3xl">
